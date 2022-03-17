92aa66b20443010af467662a4ada58a894daf5e1
Andrii_Barsuk <Andrii_Barsuk@epam.com>
2022-03-17 14:45:49 +0200

    created app_manager

--- src/main/java/com/moneymanagerapp/controllers/RegistrationController.java
+++ src/main/java/com/moneymanagerapp/controllers/RegistrationController.java
@@ -1,12 +1,19 @@
 package com.moneymanagerapp.controllers;
 
 import com.moneymanagerapp.controllers.requests.RegistrationRequest;
+import com.moneymanagerapp.domain.User;
 import com.moneymanagerapp.services.impl.RegistrationService;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Security;
+import java.util.Objects;
 
 @RestController
 @RequestMapping(path = "/api/v1/registration")
@@ -18,6 +25,17 @@ public class RegistrationController {
     @PostMapping("/signup")
     public String register(@RequestBody RegistrationRequest request) {
         return registrationService.register(request);
+    }
+
+    @PostMapping(path = "/login")
+    public @ResponseBody User getUserAuth() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null) {
+            return null;
+        }
+        Object principal = authentication.getPrincipal();
+        User user = (principal instanceof User) ? (User) principal : null;
+        return null;
     }
 
 //    @GetMapping(path = "confirm")
